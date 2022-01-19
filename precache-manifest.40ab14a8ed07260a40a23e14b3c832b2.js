42e80af6586d4a7b0ebd692a238126f0c745da25
GOWTHAM KUMAR V <vgowtham.kumar8@gmail.com>
2022-01-19 18:29:40 +0530

    Updates

---
+++ precache-manifest.40ab14a8ed07260a40a23e14b3c832b2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e429083ec0b1a52ec120cc49233fa6f",
+    "url": "/React-Bootstrap/index.html"
+  },
+  {
+    "revision": "68c0f2d3f4ce62d6a1e0",
+    "url": "/React-Bootstrap/static/css/main.1e35c2f1.chunk.css"
+  },
+  {
+    "revision": "9feeed65ea06203005a6",
+    "url": "/React-Bootstrap/static/js/2.962fc8e0.chunk.js"
+  },
+  {
+    "revision": "c4d455e0da3b131f8d5e",
+    "url": "/React-Bootstrap/static/js/3.6c748c52.chunk.js"
+  },
+  {
+    "revision": "68c0f2d3f4ce62d6a1e0",
+    "url": "/React-Bootstrap/static/js/main.c89faf1c.chunk.js"
+  },
+  {
+    "revision": "f7ab4e0521dc18bdb5a6",
+    "url": "/React-Bootstrap/static/js/runtime~main.08191155.js"
+  },
+  {
+    "revision": "915da38812380aee69c3efa1790ee06b",
+    "url": "/React-Bootstrap/static/media/Services1.915da388.jpg"
+  },
+  {
+    "revision": "1b719cfb0a8d6fa783e3af1d8130c156",
+    "url": "/React-Bootstrap/static/media/services2.1b719cfb.jpg"
+  },
+  {
+    "revision": "68ca0d5c5e62c4d099718776c203a255",
+    "url": "/React-Bootstrap/static/media/services3.68ca0d5c.jpg"
+  },
+  {
+    "revision": "332de3c92b3f5f67b54a9cf2ab30471d",
+    "url": "/React-Bootstrap/static/media/slider1.332de3c9.jpg"
+  },
+  {
+    "revision": "5dfa3827926bca42ae99be9c7a6abb9a",
+    "url": "/React-Bootstrap/static/media/slider2.5dfa3827.jpg"
+  },
+  {
+    "revision": "26623ae1ace175d392e20cf2a488dfbb",
+    "url": "/React-Bootstrap/static/media/slider3.26623ae1.jpg"
+  }
+]);
